d1c41dd25a8ee7fb0f76a4728cde20264e3ee00a
behroozam <b.hasanbg@gmail.com>
2018-08-04 15:39:39 +0430

    change base image to alpine

--- Dockerfile
+++ Dockerfile
@@ -3,19 +3,18 @@
 #
 
 # Pull base image.
-FROM ubuntu:14.04
+FROM alpine
+MAINTAINER behroozam <b.hasanbg@gmail.com>
 
+WORKDIR /var/www/html
+
+RUN apk --update upgrade && apk update && apk add curl ca-certificates && update-ca-certificates --fresh && apk add openssl
+RUN echo 'http://dl-cdn.alpinelinux.org/alpine/edge/testing' >> /etc/apk/repositories 
+RUN apk --update add \
+        nginx \
+        bash
 # Install Nginx.
-RUN \
-  apt-get update && \
-  apt-get install -y software-properties-common python-software-properties && \
-  add-apt-repository -y ppa:nginx/stable && \
-  apt-get update && \
-  apt-get install -y nginx && \
-  rm -rf /var/lib/apt/lists/* && \
-  echo "\ndaemon off;" >> /etc/nginx/nginx.conf && \
-  chown -R www-data:www-data /var/lib/nginx
-
+RUN mkdir -p /run/nginx
 # Define working directory.
 WORKDIR /etc/nginx
 
--- start-nginx.sh
+++ start-nginx.sh
@@ -4,19 +4,27 @@ MAX_BODY_SIZE=${NGINX_MAX_BODY_SIZE:-16M}
 SERVER_ADDR=${NGINX_SERVER_ADDR:?not set}
 SERVER_PORT=${NGINX_SERVER_PORT:-8080}
 
-PROXY_CONFIG_FILE=/etc/nginx/sites-available/reverse-proxy
+PROXY_CONFIG_FILE=/etc/nginx/reverse.conf
 
 echo "MAX_BODY_SIZE is ${MAX_BODY_SIZE}"
 echo "SERVER_ADDR is ${SERVER_ADDR}:${SERVER_PORT}"
 
 cat > ${PROXY_CONFIG_FILE} <<EOF
+user nobody;
+worker_processes 4;
+
+events {
+    worker_connections 1024;
+}
+
+http {
 server {
 
         listen 80 default_server;
 
         error_log /var/log/nginx/error.log;
 
-        server_name reverse-proxy;
+        server_name _;
 
         location /events {
             access_log /var/log/nginx/access.log;
@@ -36,11 +44,10 @@ server {
             proxy_pass http://${SERVER_ADDR}:${SERVER_PORT};
         }
 
+    }
 }
 EOF
+rm -rf /etc/nginx/nginx.conf
+ln -sf ${PROXY_CONFIG_FILE} /etc/nginx/nginx.conf
 
-rm /etc/nginx/sites-enabled/*
-
-ln -sf ${PROXY_CONFIG_FILE} /etc/nginx/sites-enabled/default
-
-nginx
+nginx -g 'daemon off;'
